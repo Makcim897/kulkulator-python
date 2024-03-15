0bfdc4a7dae16746d9e07c0a774b98705825a387
Makcim897 <152578968+Makcim897@users.noreply.github.com>
2024-03-15 01:02:37 -0200

    kulkulator-python
    kulkulator-python

---
+++ model.py
@@ -0,0 +1,15 @@
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '**':
+        return num1 ** num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "ПЛЮНЬ БЯКУ!!!"
+def square()
---
+++ view.py
@@ -0,0 +1,8 @@
+def get_user_input():
+    num1 = float(input("Введите первое число: "))
+    operator = input("Введите операцию (+, -, *, /,**): ")
+    num2 = float(input("Введите второе число: "))
+    return num1, operator, num2
+
+def display_result(result):
+    print(f"Результат: {result}")
